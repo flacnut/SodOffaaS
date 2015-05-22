2bca1e62c2e082190ed800c3a7bfb320935b6e68
Phani Raj <phanirajuyn@gmail.com>
2015-05-22 15:35:28 -0700

    Added bugger off route and spruced up UX

--- app/resource/sodoffController.js
+++ app/resource/sodoffController.js
@@ -6,6 +6,7 @@ var BaseRes = require('./base_res')
 var SodOff = module.exports = BaseRes.extend({
   route: function (app) {
     app.get('/sod/:off/:from', _.bind(this.sodOff, this));
+    app.get('/bugger/:off/:from', _.bind(this.buggerOff, this));
   },
 
   sodOff : function (req, res) {
@@ -14,6 +15,17 @@ var SodOff = module.exports = BaseRes.extend({
     {
       body: {
         message :util.format(constants.templates.SodOff, toName),
+        signature : fromName
+      }
+    });
+  },
+
+  buggerOff: function (req, res) {
+    var fromName = req.params.from, toName = req.params.off;
+    res.render('app/index.html',
+    {
+      body: {
+        message :util.format(constants.templates.BuggerOff, toName),
         signature : fromName
       }
     });
